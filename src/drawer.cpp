4754ec33bb44ef8a5dbcee24a5d04d21475e49ae
dmnunez1993 <dmnunez1993@gmail.com>
2017-10-16 00:24:21 -0300

    added shape switcher

--- src/drawer.cpp
+++ src/drawer.cpp
@@ -10,6 +10,9 @@
 #include <vector>
 #include <stdlib.h>
 
+#include <turtlesim_drawer/Stat.h>
+#include <turtlesim_drawer/Shape.h>
+
 #define PI 3.141592
 
 using namespace std;
@@ -38,6 +41,7 @@ enum State
     STOP_FORWARD,
     TURN,
     STOP_TURN,
+    IDLE,
     PAUSE
 };
 
@@ -69,6 +73,7 @@ class Path  {
         double getCurrentDistance(void);
         double getCurrentAngle(void);
         void updateGoal(void);
+        void reset(void);
         bool finished(void);
 };
 
@@ -97,9 +102,14 @@ bool Path::finished(void)   {
     return current_goal >= amount_of_goals;
 }
 
+void Path::reset(void)  {
+    current_goal = 0;
+    amount_of_goals = distances.size();
+}
 
 class Drawer    {
     private:
+        ros::NodeHandle nh;
         State d_state;
         State d_last_state;
         bool d_first_goal_set;
@@ -107,23 +117,37 @@ class Drawer    {
         turtlesim::Pose d_goal;
         Path path;
     public:
-        Drawer();
+        Drawer(ros::NodeHandle);
         void setPath(Path);
+        void setupDrawer(void);
         void poseCallback(const turtlesim::PoseConstPtr&);
         bool hasReachedGoal(void);
         bool hasStopped(void);
+        bool isPaused(void);
+        bool isRunning(void);
+        bool isStarted(void);
         bool rotateClockwise(double, double);
         void printGoal(void);
         void commandTurtle(ros::Publisher, float, float);
+        void publishStat(ros::Publisher);
         void stopForward(ros::Publisher);
         void stopTurn(ros::Publisher);
         void forward(ros::Publisher);
         void turn(ros::Publisher);
-        void timerCallback(const ros::TimerEvent&, ros::Publisher);
+        void pause(ros::Publisher);
+        void stop(ros::Publisher);
+        void reset(void);
+        void timerCallback(const ros::TimerEvent&, ros::Publisher, ros::Publisher);
+        bool startCallback(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
+        bool stopCallback(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
+        bool pauseCallback(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
+        bool resumeCallback(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
+        bool shapeCallback(turtlesim_drawer::Shape::Request&, turtlesim_drawer::Shape::Response&);
         void run(void);
 };
 
-Drawer::Drawer()   {
+Drawer::Drawer(ros::NodeHandle node_handle)   {
+    nh = node_handle;
     d_state = FORWARD;
     d_last_state = FORWARD;
     d_first_goal_set = false;
@@ -133,8 +157,19 @@ void Drawer::setPath(Path new_path) {
     path = new_path;
 }
 
+void Drawer::setupDrawer()  {
+    path.reset();
+    d_first_goal_set = true;
+    d_state = FORWARD;
+    d_goal.x = cos(d_pose->theta) * path.getCurrentDistance() + d_pose->x;
+    d_goal.y = sin(d_pose->theta) * path.getCurrentDistance() + d_pose->y;
+    d_goal.theta = d_pose->theta;
+    printGoal();
+}
+
 void Drawer::poseCallback(const turtlesim::PoseConstPtr& pose)  {
     d_pose = pose;
+
 }
 
 bool Drawer::hasReachedGoal()   {
@@ -154,6 +189,18 @@ bool Drawer::hasStopped()   {
     return d_pose->angular_velocity < 0.00005 && d_pose->linear_velocity < 0.00005;
 }
 
+bool Drawer::isPaused() {
+    return d_state == PAUSE;
+}
+
+bool Drawer::isRunning()    {
+    return d_state != PAUSE;
+}
+
+bool Drawer::isStarted()  {
+    return d_state != IDLE;
+}
+
 bool Drawer::rotateClockwise(double goal, double source) {
     if ((angle_to_degrees(goal) - angle_to_degrees(source) + 360) % 360 < 180)   {
         return true;
@@ -171,6 +218,13 @@ void Drawer::commandTurtle(ros::Publisher twist_pub, float linear, float angular
     twist.linear.x = linear;
     twist.angular.z = angular;
     twist_pub.publish(twist);
+}
+
+void Drawer::publishStat(ros::Publisher stat_pub)  {
+    turtlesim_drawer::Stat stat;
+    stat.paused = isPaused();
+    stat.started = isStarted();
+    stat_pub.publish(stat);
 }
 
 void Drawer::stopForward(ros::Publisher twist_pub)  {
@@ -193,7 +247,9 @@ void Drawer::stopTurn(ros::Publisher twist_pub) {
         path.updateGoal();
         if (path.finished())    {
             ROS_INFO("DONE");
-            ros::shutdown();
+            ROS_INFO("READY");
+            //ros::shutdown();
+            d_state = IDLE;
             return;
         }
         d_goal.x = cos(d_pose->theta) * path.getCurrentDistance() + d_pose->x;
@@ -231,18 +287,27 @@ void Drawer::turn(ros::Publisher twist_pub) {
     }
 }
 
-void Drawer::timerCallback(const ros::TimerEvent&, ros::Publisher twist_pub)    {
+void Drawer::pause(ros::Publisher twist_pub)    {
+    if (isPaused())   {
+        commandTurtle(twist_pub, 0, 0);
+    }
+}
+
+void Drawer::stop(ros::Publisher twist_pub) {
+    if (!isStarted())   {
+        commandTurtle(twist_pub, 0, 0);
+    }
+}
+
+void Drawer::reset()    {
+    ros::ServiceClient reset = nh.serviceClient<std_srvs::Empty>("reset");
+    std_srvs::Empty empty;
+    reset.call(empty);
+}
+
+void Drawer::timerCallback(const ros::TimerEvent&, ros::Publisher twist_pub, ros::Publisher stat_pub)    {
     if (!d_pose)    {
         return;
-    }
-
-    if (!d_first_goal_set)  {
-        d_first_goal_set = true;
-        d_state = FORWARD;
-        d_goal.x = cos(d_pose->theta) * path.getCurrentDistance() + d_pose->x;
-        d_goal.y = sin(d_pose->theta) * path.getCurrentDistance() + d_pose->y;
-        d_goal.theta = d_pose->theta;
-        printGoal();
     }
 
     if (d_state == FORWARD) {
@@ -253,23 +318,114 @@ void Drawer::timerCallback(const ros::TimerEvent&, ros::Publisher twist_pub)    
         turn(twist_pub);
     }   else if (d_state == STOP_TURN)  {
         stopTurn(twist_pub);
+    }   else if (d_state == PAUSE)  {
+        pause(twist_pub);
+    }   else if (d_state == IDLE)   {
+        stop(twist_pub);
     }
+
+    publishStat(stat_pub);
+}
+
+bool Drawer::startCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)  {
+    if (!isStarted())    {
+        ROS_INFO("Starting...");
+        reset();
+        ros::Duration(1).sleep(); //Waits for pose update
+        setupDrawer();
+        d_last_state = d_state;
+        d_state = FORWARD;
+    }   else    {
+        ROS_INFO("Already started!!");
+    }
+
+    return true;
+}
+
+bool Drawer::stopCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)  {
+    if (isRunning())    {
+        d_last_state = d_state;
+        d_state = IDLE;
+        ROS_INFO("Stopping...");
+        ROS_INFO("READY");
+    }   else    {
+        ROS_INFO("Already paused!!");
+    }
+
+    return true;
+}
+
+bool Drawer::pauseCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)  {
+    if (isStarted())    {
+        if (isRunning())    {
+            d_last_state = d_state;
+            d_state = PAUSE;
+            ROS_INFO("Pausing...");
+        }   else    {
+            ROS_INFO("Already paused!!");
+        }
+    }   else    {
+        ROS_INFO("Drawer not running!!");
+    }
+
+    return true;
+}
+
+bool Drawer::resumeCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)   {
+    if (isStarted())    {
+        if (isPaused()) {
+            d_state = d_last_state;
+            d_last_state = PAUSE;
+            ROS_INFO("Resuming...");
+        }   else    {
+            ROS_INFO("Already running!!");
+        }
+    }   else    {
+        ROS_INFO("Drawer not running!!");
+    }
+
+    return true;
+}
+
+bool Drawer::shapeCallback(turtlesim_drawer::Shape::Request& request, turtlesim_drawer::Shape::Response& response)  {
+    cout << request.shape << endl;
+    if (request.shape=="star")  {
+        Path path(star_distances, star_angles);
+        setPath(path);
+        response.current_shape = "star";
+    }   else if (request.shape=="square") {
+        Path path(square_distances, square_angles);
+        setPath(path);
+        response.current_shape = "square";
+    }   else if (request.shape=="triangle") {
+        Path path(triangle_distances, triangle_angles);
+        setPath(path);
+        response.current_shape = "triangle";
+    }
+
+    return true;
 }
 
 void Drawer::run() {
-    ros::NodeHandle nh;
     ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, &Drawer::poseCallback, this);
     ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
-    ros::ServiceClient reset = nh.serviceClient<std_srvs::Empty>("reset");
-    ros::Timer timer = nh.createTimer(ros::Duration(0.016), boost::bind(&Drawer::timerCallback, this, _1, twist_pub));
-    std_srvs::Empty empty;
-    reset.call(empty);
+    ros::Publisher stat_pub = nh.advertise<turtlesim_drawer::Stat>("turtlesim_drawer/stat", 1);
+    ros::ServiceServer start_service = nh.advertiseService("turtlesim_drawer/start", &Drawer::startCallback, this);
+    ros::ServiceServer stop_service = nh.advertiseService("turtlesim_drawer/stop", &Drawer::stopCallback, this);
+    ros::ServiceServer pause_service = nh.advertiseService("turtlesim_drawer/pause", &Drawer::pauseCallback, this);
+    ros::ServiceServer resume_service = nh.advertiseService("turtlesim_drawer/resume", &Drawer::resumeCallback, this);
+    ros::ServiceServer shape_service = nh.advertiseService("turtlesim_drawer/shape", &Drawer::shapeCallback, this);
+    ros::Timer timer = nh.createTimer(ros::Duration(0.016), boost::bind(&Drawer::timerCallback, this, _1, twist_pub, stat_pub));
+    reset();
+    ROS_INFO("READY");
+    d_state = IDLE;
     ros::spin();
 }
 
 int main(int argc, char** argv)  {
     ros::init(argc, argv, "drawer_node", ros::init_options::AnonymousName);
-    Drawer drawer;
+    ros::NodeHandle nh;
+    Drawer drawer(nh);
     Path path(star_distances, star_angles);
     drawer.setPath(path);
     drawer.run();
